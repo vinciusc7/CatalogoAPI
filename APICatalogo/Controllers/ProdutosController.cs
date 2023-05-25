80bc10b93144cac0a01341b73bd192b454ddef28
Vinicius Chaves <vinciusc7@gmail.com>
2023-05-25 17:43:13 -0300

    Criado ProdutoController e 2 metodos HTTPGet

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using APICatalogo.Context;
+using APICatalogo.Models;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Não existem produtos cadastrados!");
+            }
+            return produtos;
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+
+            var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+            if (produto is null)
+            {
+                return BadRequest("Produto não encontrado!");
+            }
+            return produto;
+        }
+    }
+}
