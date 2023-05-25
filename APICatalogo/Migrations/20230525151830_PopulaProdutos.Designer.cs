aa1c688bfa85771084da452df44f04ca52bba0ed
Vinicius Chaves <vinciusc7@gmail.com>
2023-05-25 12:41:18 -0300

    Utilizado migrations para popular Produtos

--- APICatalogo/Migrations/20230525151830_PopulaProdutos.Designer.cs
+++ APICatalogo/Migrations/20230525151830_PopulaProdutos.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APICatalogo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230525151830_PopulaProdutos")]
+    partial class PopulaProdutos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -71,7 +74,7 @@ namespace APICatalogo.Migrations
                         .HasColumnType("varchar(85)");
 
                     b.Property<decimal>("Preco")
-                        .HasColumnType("decimal(10,2)");
+                        .HasColumnType("decimal(11,2)");
 
                     b.HasKey("ProdutoId");
 
